88ed665c05c44e3f3f2afc6fc345dbc7aedf3c34
delower68 <hdelower68@gmail.com>
2022-09-15 06:00:15 +0800

    takentime debug done

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -118,7 +118,7 @@ const start = () => {
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
-      startTime = new Date().getSeconds();
+      startTime = new Date().getTime();
     }
     count--;
   }, );
@@ -132,9 +132,9 @@ displayHistory();
 
 // Show typing time spent
 setInterval(() => {
-  const currentTime = new Date().getSeconds();
-  const timeSpent = (currentTime) - (startTime)%60 ;
+  const currentTime = new Date().getTime();
+  const timeSpent = ((currentTime) - (startTime))/1000 ;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = parseInt(`${startTime ? timeSpent : 0}`) + ' seconds';
 }, 1000);
